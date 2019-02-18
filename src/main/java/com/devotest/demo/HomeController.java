af249dd35657980c4d64fa9fd7275dfede242984
yajbar <younes.ajbar@student.ehb.be>
2019-02-18 13:38:03 +0100

    First commit

---
+++ src/main/java/com/devotest/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.devotest.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/app")
+public class HomeController {
+
+
+   @GetMapping({"","/"})
+    public String index (){
+    return "Welcome to my application.";
+    }
+
+    @GetMapping("/{id}")
+    public String geefId(@PathVariable int id){
+        return "The given id is " + id;
+    }
+}
